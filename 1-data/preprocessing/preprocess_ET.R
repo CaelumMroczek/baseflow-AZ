a4aab09ec4a3210ff6553945861d4f5bcfc46dfd
Caelum Mroczek <cmroczek@nau.edu>
2024-09-03 13:16:59 -0700

    ET Preprocessing Workaround
    This workaround is necessary for M1 Macs, won't allow mikejohnson51/climateR to install and run.

--- 1-data/preprocessing/preprocess_ET.R
+++ 1-data/preprocessing/preprocess_ET.R
@@ -11,34 +11,75 @@ prism::prism_set_dl_dir(here::here("1-data/raw/PRISM"))
 #AZ state shapefile
 AZ <- AOI::aoi_get(state = "AZ")
 
-#GW Basins shapefile
-huc8_shape <- raster::shapefile(here::here("1-data/raw/shapefile/huc8.shp"))
-
 #Format to pull produce raster:
 ppt_2013<- prism::pd_to_file(prism::prism_archive_subset("ppt", "annual", years = 2013))
 ppt_2013_rast <- raster::raster(ppt_2013)
 
+#GW Basins shapefile
+huc8_shape <- raster::shapefile(here::here("1-data/raw/shapefile/huc8.shp"))
+
 #Set CRS to the same
-huc8_shape <- sp::spTransform(huc8_shape, raster::crs(ppt_2013_rast))
+huc8_shape <- spTransform(huc8_shape, raster::crs(ppt_2013_rast))
 
 huc8_annualET <- data.frame(HUC8 = huc8_shape$HUC8) #dataframe with HUC8 numbers in order
 
+####################################################################################################
+##--Following this: https://tmieno2.github.io/R-as-GIS-for-Economists/gridMET.html--##
+
+#gridMET etr = grass reference crop; pet = alfalfa reference crop
+downloader::download(
+  url = "http://www.northwestknowledge.net/metdata/data/pet_2015.nc",
+  destfile = "1-data/raw/gridMET/pet_2015.nc",
+  mode = 'wb'
+)
+
+pet_2015 <- terra::rast("1-data/raw/gridMET/pet_2015.nc")
+huc8_shape <- spTransform(huc8_shape, crs(pet_2015))
+
+
+####################
+pet_by_huc <-
+  exact_extract(pet_2015, huc8_shape, progress = FALSE) %>%
+  rbindlist(idcol = "rowid")
+
+pet_by_huc <-
+  #--- wide to long ---#
+  melt(pet_by_huc, id.var = c("rowid", "coverage_fraction")) %>%
+  #--- remove observations with NA values ---#
+  .[!is.na(value), ]
+
+
+pet_by_id <- pet_by_huc %>%
+  group_by(rowid,variable) %>%
+  summarise(pet_aw = (sum(value * coverage_fraction)) / (sum(coverage_fraction)))
+
+pet <- pet_by_id %>%
+  group_by(rowid) %>%
+  summarise(pet_anual = sum(pet_aw))
+
+
+pet_by_id <- pet_by_huc %>%
+  #--- convert from character to numeric  ---#
+  mutate(rowid = as.numeric(rowid)) %>%
+  #--- group summary ---#
+  group_by(rowid) %>%
+  summarise(pet_aw = (sum(value * coverage_fraction)) / (sum(coverage_fraction)))
+
+####################################################################################################
+huc8_annualET <- cbind(huc8_annualET, test)
+
+##############
 for (year in 1990:2022){ #only goes to 1979
 
   ##########
   d <- climateR::getGridMET(AZ, #shape of AZ
                   varname = "pet", #actual ET
                   startDate = paste(year,"01","01",sep = "-"),
-                  endDate= paste(year,"12","31", sep = "-"))
+                  endDate = paste(year,"12","31", sep = "-"))
 
 
   #calculate mean annual ET
-  ################################each day of the year has a `min value` and `max value` for pet
-  ################################should I average each day? need to code that out to replace the below line of code
   summed_values <- terra::app(d$daily_mean_reference_evapotranspiration_grass, sum)
-
-
-
 
   z <- exact_extract(summed_values, GWBasins, fun = "mean", append_cols = "BASIN_NAME")
 
--- scratchpad.md
+++ scratchpad.md
@@ -1,9 +1,17 @@
 ## 30 August 2024
-- produced and reworked huc8 precipitation and temperature data in the new project format
-- began implementing gridMET data in preprocessing
-  - Current issue: each day of the year has a `min value` and `max value` for pet
-  - Assumedly I should take the mean between the two?
+
+-   produced and reworked huc8 precipitation and temperature data in the new project format
+-   began implementing gridMET data in preprocessing
+    -   Current issue: each day of the year has a `min value` and `max value` for pet
+    -   Assumedly I should take the mean between the two?
 
 ## 31 August 2024
-- having issues accessing `climateR`, can't run `getGridMET()`
-  - need to figure out another way to access and clip gridMET data
+
+-   having issues accessing `climateR`, can't run `getGridMET()`
+    -   need to figure out another way to access and clip gridMET data
+
+## 2 September 2024
+
+-   worked through this <https://tmieno2.github.io/R-as-GIS-for-Economists/gridMET.html> workflow to access gridMET data
+    -   exactextractr ReadMe provides explanations on how to 'natively' calculate the coverage-weighted average of PET in each HUC8 basin
+    -   Each gridMET cell has an associated fraction of coverage within each HUC8; so the sum of raster values within the polygon, accounting for coverage fraction is required
