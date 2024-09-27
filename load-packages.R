9c8eff0fd282ca0a485dd8aa860a7090c5a20168
cmroczek <cmroczek@nau.edu>
2024-09-27 12:43:36 -0700

    Update load-packages.R

--- load-packages.R
+++ load-packages.R
@@ -1,30 +1,28 @@
-packageURL <- "https://cran.r-project.org/src/contrib/Archive/EcoHydRology/EcoHydRology_0.4.12.1.tar.gz"
-install.packages(packageURL, repos=NULL, type="source")
+ecohydro <- "https://cran.r-project.org/src/contrib/Archive/EcoHydRology/EcoHydRology_0.4.12.1.tar.gz"
+install.packages(ecohydro, repos=NULL, type="source")
 
-library(AOI)
-library(climateR)
-library(data.table)
-library(dataRetrieval)
-library(dplyr)
-library(EcoHydRology)
-library(exactextractr)
-library(ggplot2)
-library(here)
-library(lubridate)
-library(raster)
-library(readr)
-library(renv)
-library(rmarkdown)
-library(sf)
-library(sp)
-library(stringr)
-library(stringr)
-library(sf)
-library(tidyverse)
-library(terra)
-library(elevatr)
-library(boot)
-library(xgboost)
-library(caret)
-library(ggthemes)
-library(hydroGOF)
+install.packages('remotes')
+library('remotes')
+
+remotes::install_github("mikejohnson51/AOI")
+#remotes::install_github("mikejohnson51/climateR") Problem causer on Mac
+
+# Create a vector of all the package names
+packages <- c("AOI", "data.table", "dataRetrieval", "dplyr","exactextractr",
+              "ggplot2", "here", "lubridate", "raster", "readr",
+              "renv", "rmarkdown", "sf", "sp", "stringr", "tidyverse", "terra",
+              "elevatr", "boot", "xgboost", "caret", "ggthemes", "hydroGOF")
+
+# Install packages that are not already installed
+install_if_missing <- function(pkg) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg, dependencies = TRUE)
+    library(pkg, character.only = TRUE)
+  }
+}
+
+# Apply the function to all packages
+invisible(lapply(packages, install_if_missing))
+
+# Load all the packages
+lapply(packages, library, character.only = TRUE)
